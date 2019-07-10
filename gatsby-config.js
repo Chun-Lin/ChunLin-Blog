d58e8f754e4def06e1c9f3f87b8fd0b4fda73c92
Chun-Lin <wulin40063@gmail.com>
2019-07-10 14:45:38 +0800

    fix: 🐛 add GATSBY_ prefix to the env vars

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,11 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require("dotenv").config()
+
+console.log("LOG: process.env.CONTENTFUL_SPACE_ID", process.env.GATSBY_CONTENTFUL_SPACE_ID)
+console.log("LOG: process.env.CONTENTFUL_ACCESS_TOKEN", process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN)
+
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -12,12 +17,12 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-source-contentful`,
+      resolve: "gatsby-source-contentful",
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
       },
     },
-    'gatsby-plugin-sass',
-  ]
+    "gatsby-plugin-sass",
+  ],
 }
