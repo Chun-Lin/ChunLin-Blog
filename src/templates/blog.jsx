f9ea8e35171d7950ca7927df3761bce4bf35dbf6
Chun-Lin <wulin40063@gmail.com>
2019-07-16 11:53:42 +0800

    feat: 🎸 add title and meta-data to every blog

--- src/templates/blog.jsx
+++ src/templates/blog.jsx
@@ -4,6 +4,7 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import Img from "gatsby-image"
 
 import Layout from "../components/Layout"
+import Head from "../components/Head"
 
 export const query = graphql`
   query($slug: String!) {
@@ -31,8 +32,12 @@ const Blog = props => {
   const { title, publishedDate, body } = props.data.contentfulChunLinBlog
   return (
     <Layout contentStyle="grey">
+      <Head title={title} metaName={title} metaContent={title} />
       {props.data.allContentfulAsset.edges[0] ? (
-        <Img fluid={props.data.allContentfulAsset.edges[0].node.fluid} alt={title}/>
+        <Img
+          fluid={props.data.allContentfulAsset.edges[0].node.fluid}
+          alt={title}
+        />
       ) : null}
       <h1>{title}</h1>
       <p>{publishedDate}</p>
